2610e20e7c307079368195924af4bd340329cdaf
Claudio De Angelis <claudio@enboarder.com>
2021-03-31 18:37:38 +1100

    feat(get-arnie-quotes): implementation (I'll be back)

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,21 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
 
-const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+const getArnieQuotes = async (urls = []) => {
+  /** move out if shared across multiple utils/controllers */
+  const statusKeyMap = {
+    200: "Arnie Quote",
+    500: "FAILURE",
+  };
+  const quotes = await Promise.all(
+    urls.map(async (url) => {
+      const { status, body: bodyString } = await httpGet(url);
+      const quote = JSON.parse(bodyString);
+      const { message: responseValue } = quote;
+      const responseKey = statusKeyMap[status];
+      return { [responseKey]: responseValue };
+    })
+  );
+  return quotes;
 };
 
 module.exports = {
