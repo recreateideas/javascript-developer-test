f441cb10149f5a3f3a3d38fbe51087304fa65414
Claudio De Angelis <claudio@enboarder.com>
2021-04-06 12:11:36 +1000

    fix(get-arnie-quotes): implementation fixes (I told you I'd be back)

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,21 +1,22 @@
 const { httpGet } = require("./mock-http-interface");
 
-const getArnieQuotes = async (urls = []) => {
+const getArnieQuotes = (urls = []) => {
   /** move out if shared across multiple utils/controllers */
   const statusKeyMap = {
     200: "Arnie Quote",
-    500: "FAILURE",
+    default: "FAILURE",
   };
-  const quotes = await Promise.all(
+  const quotesPromise = Promise.all(
     urls.map(async (url) => {
       const { status, body: bodyString } = await httpGet(url);
       const quote = JSON.parse(bodyString);
       const { message: responseValue } = quote;
-      const responseKey = statusKeyMap[status];
+      const { [status]: success, default: failure } = statusKeyMap;
+      const responseKey = success || failure;
       return { [responseKey]: responseValue };
     })
   );
-  return quotes;
+  return quotesPromise;
 };
 
 module.exports = {
